154a6d4c42516c791bb2445dce67bdee452e5064
FoelliX <my@FoelliX.de>
2020-12-09 07:47:37 +0100

    Readded check operator as module

--- AQLCheckOperator/mvn/build.bat
+++
@@ -1,4 +0,0 @@
-cd target/build
-
-"%JAVA_HOME%\bin\jar.exe" uf %1 tool.properties
-del tool.properties
--- AQLCheckOperator/mvn/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd target/build
-
-zip -u $1 tool.properties
-rm tool.properties
